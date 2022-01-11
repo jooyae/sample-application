c2dab6176d890fd12ea6a80111cf28da4d122959
jooyae <jooyae3970@naver.com>
2022-01-11 13:44:01 +0900

    [chore] Set KotlinDSL

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.1")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://devrepo.kakao.com/nexus/content/groups/public/")
+        maven {
+            setUrl("https://naver.jfrog.io/artifactory/maven/")
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "ReminiscenceWinter"
+include (":app")
+
