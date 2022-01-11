79f4c74f48752dd63969e2929a296dbbcbe45c70
jooyae <jooyae3970@naver.com>
2022-01-11 18:33:01 +0900

    [fix] DSL 문법 오류 수정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -3,6 +3,9 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 plugins {
     `kotlin-dsl`
 }
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter")
+}
 
 repositories {
     mavenCentral()
--- data/src/test/java/org/android/data/ExampleUnitTest.kt
+++ data/src/test/java/org/android/data/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package org.android.data
 
-import org.junit.Test
+import groovy.test.GroovyTestCase.assertEquals
+import org.gradle.internal.impldep.org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
