b52f0bdf388472afeac30e60b1d0af4c3e91e2fa
jooyae <jooyae3970@naver.com>
2022-01-11 17:58:33 +0900

    [feat] CoroutineScope Splash

--- app/src/main/java/org/android/reminiscencewinter/presentation/splash/SplashFragment.kt
+++ app/src/main/java/org/android/reminiscencewinter/presentation/splash/SplashFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import org.android.reminiscencewinter.R
 import org.android.reminiscencewinter.databinding.FragmentSplashBinding
 import org.android.reminiscencewinter.presentation.util.AutoClearedValue
+import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
-class SplashFragment : Fragment(){
+class SplashFragment : Fragment(), CoroutineScope{
     private var binding by AutoClearedValue<FragmentSplashBinding>()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + Job()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,5 +31,16 @@ class SplashFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        launchSplash()
+    }
+
+    private fun launchSplash(){
+        launch {
+            delay(1500)
+            withContext(Dispatchers.Main){
+                findNavController().navigate(R.id.action_splashFragment_to_mainFrameFragment)
+            }
+        }
     }
 }
