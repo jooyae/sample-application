b483250467dcb29cbd23a715ae5149279b4a56ab
jooyae <jooyae3970@naver.com>
2022-01-11 17:10:55 +0900

    [feat]Set Bottom Navigation Menu

---
+++ app/src/main/java/org/android/reminiscencewinter/MemoryFragment.kt
@@ -0,0 +1,28 @@
+package org.android.reminiscencewinter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import org.android.reminiscencewinter.databinding.FragmentMemoryBinding
+import org.android.reminiscencewinter.util.AutoClearedValue
+
+@AndroidEntryPoint
+class MemoryFragment : Fragment(){
+    private var binding by AutoClearedValue<FragmentMemoryBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = FragmentMemoryBinding.inflate(inflater, container, false)?.let {
+        binding = it
+        it.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
