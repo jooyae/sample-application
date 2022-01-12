b8063eb869724f5a95f3c8e9641789d05d3f1cde
jooyae <jooyae3970@naver.com>
2022-01-12 15:19:10 +0900

    [chore] Delete Theme File

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -3,9 +3,6 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 plugins {
     `kotlin-dsl`
 }
-dependencies {
-    implementation("org.junit.jupiter:junit-jupiter")
-}
 
 repositories {
     mavenCentral()
